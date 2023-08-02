a936cee56a130a49d495cb924cf5ea96bdf319df
AURORA\Ehsan <seyedmohammadehsan.sajadi@haw-hamburg.de>
2023-08-03 00:41:47 +0200

    add crud methods in DAO interface and DAO Impl

--- src/main/java/com/luv2code/sprigboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ src/main/java/com/luv2code/sprigboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -23,4 +23,28 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO{
         List<Employee> employees = theQuery.getResultList();
         return employees;
     }
+
+    @Override
+    public Employee findById(int theId) {
+        // get employee
+        Employee theEmployee = entityManager.find(Employee.class,theId);
+        // return employee
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        // save employee
+        Employee dbEmployee = entityManager.merge(theEmployee);
+        //return employee
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        // find the employee
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        // delete employee
+        entityManager.remove(theEmployee);
+    }
 }
